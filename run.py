ad92ce7fddfa7f0ab9cab5b81a0469bb2a086778
leoniemclaughlin7 <>
2023-06-16 11:22:16 +0000

    Add function get_albums, which reads the album data from the csv

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import csv
 from pprint import pprint
 
-ALBUMS = 'albumlist.csv'
-all_albums = []
-
 
 class Album:
     def __init__(self, number, year, album, artist, genre, subgenre):
@@ -16,3 +13,17 @@ class Album:
         self.artist = artist
         self.genre = genre
         self.subgenre = subgenre
+
+
+def get_albums():
+    """
+    Returns all the albums in the dataset in a list
+    https://realpython.com/python-csv/
+    """
+    with open('albumlist.csv', encoding='latin1') as album_list:
+        csv_reader = csv.reader(album_list, delimiter=',')
+        for albums in csv_reader:
+            return albums
+
+
+get_albums()
