edd528a6d9e61f305959891ca1f298f3901c439c
leoniemclaughlin7 <>
2023-06-22 19:09:27 +0000

    refactored remove_owned to fix albums not being removed

--- owned.py
+++ owned.py
@@ -45,24 +45,26 @@ def remove_owned():
     """
     Removes album for list of owned albums
     https://datagy.io/python-list-pop-remove-del-clear/
-    https://stackoverflow.com/questions/9553638/find-the-index-of-an-item-in-a-list-of-lists
-    https://www.w3schools.com/python/ref_func_reversed.asp
     """
+    
     try:
         if not owned_album:
             cprint("You have not added anything to this list yet!" , "red")  
         else:
-            album_ranking = int(input(colored("Please input the ranking of the album you would "
-                          "like to remove:\n", "light_magenta")))
-        if album_ranking < 1 or album_ranking > 500:
-            raise ValueError
-        removed_albums = []
-        for i, album in enumerate(owned_album):
-            if album_ranking in album:
-                removed_albums.append(i)
+            album_ranking = input(colored("Please input the ranking of the album you would "
+                          "like to remove:\n", "light_magenta"))
+            if int(album_ranking) < 1 or int(album_ranking) > 500:
+                raise ValueError
+            removed_albums = []
+            for album in owned_album:
+                if album_ranking in album:
+                    removed_albums.append(album)
+                    for lst in removed_albums:
+                        if lst in owned_album:
+                            owned_album.remove(lst)
+            if removed_albums:
+                cprint("Album has been removed from owned list" , "light_yellow")       
             else:
                 cprint("Album not in owned list!" , "red")
-        for index in reversed(removed_albums):
-            owned_album.pop(index)
     except ValueError:
         cprint("Invalid input: Please enter a number between 1 and 500" , "red") 
